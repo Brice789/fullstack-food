575bebd3ee32e039f401208a1348997cb8c85055
Brice789 <kireche.brice@gmail.com>
2023-05-30 17:07:11 +0200

    backround

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -13,6 +13,16 @@ const Login = () => {
       />
 
       {/* rest of your code */}
+
+      <div
+        className="absolute top-0 left-0 w-[25%] h-full bg-gray-300 opacity-60"
+        style={{ zIndex: "10" }}
+      ></div>
+
+   {/* <div
+  className="flex flex-col items-center bg-gray-400 w-[80%] md:w-508 h-full z-10 backdrop-blur-md"
+   ></div> */}
+
     </div>
   );
 };
