95543964bb810928e40fb614e57281d93e3b10f6
Brice789 <kireche.brice@gmail.com>
2023-06-03 14:29:36 +0200

    --

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -15,12 +15,28 @@ const Login = () => {
       {/* rest of your code */}
 
       <div
-        className="absolute top-0 left-0 w-[25%] h-full bg-gray-300 opacity-60"
+        className="absolute top-0 left-0 w-[27%] h-full bg-gray-100 opacity-60"
         style={{ zIndex: "10" }}
       ></div>
 
+
+      <div className="flex items-center justify-start gap-4 w-full">
+
+        <img src ={avatar} className="w-8" alt =""></img>
+      </div>
+
+
+
+        {/* <div
+        className="flex flex-col items-center bg-gray-400 w-[80%] md:w-508 h-full z-10 
+        backdrop-blur-md p-4 px-4 py-12"
+
+        
+      ></div> */}
+
    {/* <div
-  className="flex flex-col items-center bg-gray-400 w-[80%] md:w-508 h-full z-10 backdrop-blur-md"
+  className="flex flex-col items-center bg-gray-400 w-[80%] md:w-508 h-full z-10 
+  backdrop-blur-md p-4 px-4 py-12"
    ></div> */}
 
     </div>
